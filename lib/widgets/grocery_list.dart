7e516c6408b2b8bf56779f6c553494a515762572
Utkarsh Mishra <utkarsh971762@gmail.com>
2024-03-28 00:57:54 +0530

    Final Commit

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -16,52 +16,107 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
-
+  var _isLoading = true;
+  // late Future<List<GroceryItem>> _loadedItems;
+  String? _error;
   @override
   void initState() {
     super.initState();
-    _loadItems();
+    // _loadedItems = _loadItems();
   }
 
+  // Future<List<GroceryItem>> _loadItems() async {
   void _loadItems() async {
     final url = Uri.https(
         'flutter-prpn-default-rtdb.firebaseio.com', 'shopping-list.json');
-    final response = await http.get(url);
-    final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> _loadedItems = [];
-    for (final item in listData.entries) {
-      final category = categories.entries
-          .firstWhere(
-              (catItem) => catItem.value.title == item.value['category'])
-          .value;
-      _loadedItems.add(GroceryItem(
-          id: item.key,
-          name: item.value['name'],
-          quantity: item.value['quantity'],
-          category: category));
+
+    try {
+      final response = await http.get(url);
+      if (response.statusCode >= 400) {
+        //with future builder
+        // throw Exception('Failed to fetch grocery items. Please try again later.');
+
+        //without future builder
+        setState(() {
+          _error = 'Failed to fetch data. Please try again later.';
+        });
+      }
+
+      if (response.body == 'null') {
+        // without future builder
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+        // with future builder
+        // return [];
+      }
+
+      final Map<String, dynamic> listData = json.decode(response.body);
+      final List<GroceryItem> loadedItems = [];
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'])
+            .value;
+        loadedItems.add(GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category));
+      }
+      //without future builder
+      setState(() {
+        _groceryItems = loadedItems;
+        _isLoading = false;
+      });
+    } catch (err) {
+      setState(() {
+        _error = 'Something went wrong!. Please try again later.';
+      });
     }
-    setState(() {
-      _groceryItems = _loadedItems;
-    });
+    //with future builder
+    // return loadedItems;
   }
 
   void _addItem() async {
-    await Navigator.of(context)
+    final newItem = await Navigator.of(context)
         .push<GroceryItem>(MaterialPageRoute(builder: (ctx) => NewItem()));
-    _loadItems();
+
+    if (newItem == null) {
+      return;
+    }
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+    final url = Uri.https('flutter-prpn-default-rtdb.firebaseio.com',
+        'shopping-list/${item.id}.json');
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    //without future builder
     Widget content = const Center(
       child: Text('No items added yet.'),
     );
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
@@ -84,6 +139,13 @@ class _GroceryListState extends State<GroceryList> {
                 ),
               ));
     }
+    //without future builder
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Groceries'),
@@ -94,7 +156,44 @@ class _GroceryListState extends State<GroceryList> {
           )
         ],
       ),
+      // without future builder
       body: content,
+
+      //with future builder just for learning about future builder
+      // body: FutureBuilder(
+      //     future: _loadedItems,
+      //     builder: (context, snapshot) {
+      //       if (snapshot.connectionState == ConnectionState.waiting) {
+      //         return const Center(child: CircularProgressIndicator());
+      //       }
+      //       if (snapshot.hasError) {
+      //         return Center(child: Text(_error!));
+      //       }
+      //       if (snapshot.data!.isEmpty) {
+      //         return const Center(
+      //           child: Text('No items added yet.'),
+      //         );
+      //       }
+      //       return ListView.builder(
+      //     itemCount: snapshot.data!.length,
+      //     itemBuilder: (ctx, index) => Dismissible(
+      //           onDismissed: (direction) {
+      //             _removeItem(snapshot.data![index]);
+      //           },
+      //           key: ValueKey(snapshot.data![index].id),
+      //           child: ListTile(
+      //             leading: Container(
+      //               width: 24,
+      //               height: 24,
+      //               color: snapshot.data![index].category.color,
+      //             ),
+      //             title: Text(
+      //               snapshot.data![index].name,
+      //             ),
+      //             trailing: Text(snapshot.data![index].quantity.toString()),
+      //           ),
+      //         ));
+      //     }),
     );
   }
 }
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,10 +19,14 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      setState(() {
+        _isSending = true;
+      });
       final url = Uri.https(
           'flutter-prpn-default-rtdb.firebaseio.com', 'shopping-list.json');
       final response = await http.post(url,
@@ -34,11 +38,15 @@ class _NewItemState extends State<NewItem> {
           }));
       // print(response.body);
       // print(response.statusCode);
-
+      final Map<String, dynamic> resData = json.decode(response.body);
       if (!context.mounted) {
         return;
       }
-      Navigator.of(context).pop();
+      Navigator.of(context).pop(GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory));
     }
   }
 
@@ -135,12 +143,21 @@ class _NewItemState extends State<NewItem> {
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     TextButton(
-                        onPressed: () {
-                          _formKey.currentState!.reset();
-                        },
+                        onPressed: _isSending
+                            ? null
+                            : () {
+                                _formKey.currentState!.reset();
+                              },
                         child: const Text('Reset')),
                     ElevatedButton(
-                        onPressed: _saveItem, child: const Text('Submit'))
+                        onPressed: _isSending ? null : _saveItem,
+                        child: _isSending
+                            ? const SizedBox(
+                                height: 15,
+                                width: 16,
+                                child: CircularProgressIndicator(),
+                              )
+                            : const Text('Submit'))
                   ],
                 )
               ],
